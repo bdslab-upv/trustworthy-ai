5557cad45207c70b810b23ee1318334a8697110b
carlosdmv7 <145647859+carlosdmv7@users.noreply.github.com>
2024-06-28 12:26:35 +0200

    Prueba 2

--- Dataset Diabetes/functions/NaiveBayesMixed.py
+++ Dataset Diabetes/functions/NaiveBayesMixed.py
@@ -7,45 +7,94 @@ class NaiveBayesMixed(BaseEstimator, ClassifierMixin):
     Mixed Naive Bayes classifier.
 
     This classifier combines Gaussian Naive Bayes for numerical features
-    and Categorical Naive Bayes for categorical features. It preprocesses
-    the categorical features using one-hot encoding.
+    and Categorical Naive Bayes for categorical features.
 
     Parameters:
     ----------
     feat_num : list of column names corresponding to numerical features.
     feat_cat : list of column names corresponding to categorical features.
+    smoothing : float, optional (default=1e-09)
+        Portion of the largest variance of all features added to variances for calculation stability.
+    alpha_value : float, optional (default=1)
+        Additive (Laplace/Lidstone) smoothing parameter for categorical features.
 
     Attributes:
     ----------
-    gaussian_nb : Gaussian Naive Bayes model for numerical features.
-    multinomial_nb : Categorical Naive Bayes model for categorical features.
-    preprocessor : Preprocessor for categorical features, performing one-hot encoding.
-    classes_ : Array of class labels.
+    gaussian_nb : GaussianNB
+        Gaussian Naive Bayes model for numerical features.
+    categorical_nb : CategoricalNB
+        Categorical Naive Bayes model for categorical features.
+    classes_ : array, shape (n_classes,)
+        Array of class labels.
     """
 
-    def __init__(self, feat_num, feat_cat):
+    def __init__(self, feat_num, feat_cat, var_smoothing=1e-09, alpha=1):
         self.feat_num = feat_num
         self.feat_cat = feat_cat
+        self.var_smoothing = var_smoothing
+        self.alpha = alpha
 
-        self.gaussian_nb = GaussianNB()
-        self.categorical_nb = CategoricalNB()
+        self.gaussian_nb = GaussianNB(var_smoothing=self.var_smoothing)
+        self.categorical_nb = CategoricalNB(alpha= self.alpha)
         self.classes_ = None  # Unknown classes until 'fit' method is called
 
     def fit(self, features, tags, sample_weight=None):
+        """
+        Fit the model according to the given training data.
+
+        Parameters:
+        ----------
+        features : pandas DataFrame, shape (n_samples, n_features)
+            Training vectors.
+        tags : array-like, shape (n_samples,)
+            Target values.
+        sample_weight : array-like, shape (n_samples,), optional
+            Weights applied to individual samples.
+
+        Returns:
+        -------
+        self : object
+            Returns self.
+        """
         numerical_features = features[self.feat_num]
         categorical_features = features[self.feat_cat]
 
         self.gaussian_nb.fit(numerical_features, tags, sample_weight=sample_weight)
         self.categorical_nb.fit(categorical_features, tags, sample_weight=sample_weight)
-        self.classes_ = np.unique(tags) # Store the classes
+        self.classes_ = np.unique(tags)  # Store the classes
 
         return self
 
     def predict(self, features):
+        """
+        Perform classification on an array of test vectors.
+
+        Parameters:
+        ----------
+        features : pandas DataFrame, shape (n_samples, n_features)
+            Test vectors.
+
+        Returns:
+        -------
+        tags_pred : array, shape (n_samples,)
+            Predicted target values for the provided data.
+        """
         return self.categorical_nb.classes_[np.argmax(self.predict_proba(features), axis=1)]
 
     def predict_proba(self, features):
+        """
+        Return probability estimates for the test vector.
 
+        Parameters:
+        ----------
+        features : pandas DataFrame, shape (n_samples, n_features)
+            Test vectors.
+
+        Returns:
+        -------
+        proba : array, shape (n_samples, n_classes)
+            Returns the probability of the sample for each class in the model.
+        """
         proba_continuous = self.gaussian_nb.predict_proba(features[self.feat_num])
         proba_categorical = self.categorical_nb.predict_proba(features[self.feat_cat])
         return proba_continuous * proba_categorical
--- Dataset Diabetes/functions/handleData.py
+++ Dataset Diabetes/functions/handleData.py
@@ -23,11 +23,11 @@ class handleData(BaseEstimator, TransformerMixin):
 
         X_encoded = preprocessor.fit_transform(X)
         feat_cat_encoded = list(preprocessor.named_transformers_['cat'].get_feature_names_out(self.feat_cat))
-        X_names_encoded = self.feat_num + feat_cat_encoded
+        self.X_names_encoded = self.feat_num + feat_cat_encoded
 
-        X_encoded = pd.DataFrame(X_encoded, columns=X_names_encoded, index=X.index)
+        X_encoded = pd.DataFrame(X_encoded, columns=self.X_names_encoded, index=X.index)
 
-        return X_encoded, X_names_encoded, feat_cat_encoded
+        return X_encoded, self.X_names_encoded, feat_cat_encoded
     
 
     def decode(self, X_encoded):
@@ -44,33 +44,38 @@ class handleData(BaseEstimator, TransformerMixin):
         return X
     
 
-    def get_random_samples(self, X, Y):
-        unique_tags = pd.unique(Y)
-        random_indexes = {tags: [] for tags in unique_tags}
+    def get_random_samples(self, X, Y, num_samples=1, specific_class= None):
+
+        if specific_class is not None:
+            unique_tags = [specific_class] if specific_class in Y.unique() else []
+        else:
+            unique_tags = pd.unique(Y)
+
+        random_samples = {tag: pd.DataFrame(columns=X.columns) for tag in unique_tags}
 
         for tag in unique_tags:
-            rdm_idx = random.choice(Y[Y == tag].index)
-            rdm_feat = X.loc[rdm_idx].values.reshape(1, -1) # Make sure it is a 2D array
-            rdm_feat = pd.DataFrame(rdm_feat, columns=X.columns)
-            random_indexes[tag]=rdm_feat
+            tag_indexes = Y[Y == tag].index
+            selected_indexes = random.sample(list(tag_indexes), min(num_samples, len(tag_indexes)))
+            selected_features = X.loc[selected_indexes]
+            random_samples[tag] = selected_features
 
-        return random_indexes
-    
+        return random_samples
 
     def perturb_sample(self, sample, X, times):
-      
         num_std_devs = X[self.feat_num].std()  # Standard deviation for numerical features
         categories = {feat: X[feat].unique() for feat in self.feat_cat}  # Unique categories for categorical features
-        perturbed_samples = pd.DataFrame(index=range(times), columns= sample.index.tolist())
+        perturbed_samples = pd.DataFrame(index=range(times), columns=sample.index.tolist())
 
         for i in range(times):
             perturbed_sample = sample.copy()
             for feat in self.feat_num:
-                perturbed_sample[feat] += np.random.normal(0, num_std_devs[feat])  # Perturb numerical data based on std deviation
-            for feat in self.feat_cat:
-                perturbed_sample[feat] = np.random.choice(categories[feat])  # Perturb categorical data
+                perturbation = np.random.normal(0, 0.05 * num_std_devs[feat])  # Perturb numerical data based on 5% of the standard deviation
+                perturbed_sample[feat] += perturbation
+            if i % 20 == 0:  # Only change categories in 5% of the samples
+                for feat in self.feat_cat:
+                    perturbed_sample[feat] = np.random.choice(categories[feat])  # Perturb categorical data
             perturbed_samples.loc[i] = perturbed_sample
-            
+
         return perturbed_samples
     
     def group_feature_importanceT(self, feature_names, feature_importance, feat_cat_encoded):
@@ -97,3 +102,17 @@ class handleData(BaseEstimator, TransformerMixin):
 
 
         return grouped_feature_importance
+    
+    def adjust_categories(self, X, X_names_encoded):
+        # Identify missing categories in the outliers
+        categories_A = set(X_names_encoded)
+        categories_B = set(X.columns)
+        missing_categories = categories_A - categories_B
+
+        for category in missing_categories:
+            X[category] = 0
+        
+        X = X.reindex(X_names_encoded, axis=1)
+
+        return X
+
